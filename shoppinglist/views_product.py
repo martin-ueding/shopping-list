651836a6fdf02cf3df6798999f957f173cd1dbe2
Martin Ueding <dev@martin-ueding.de>
2015-04-27 18:48:24 +0200

    Refactor, make buttons work, padding

--- shoppinglist/views_product.py
+++ shoppinglist/views_product.py
@@ -36,3 +36,25 @@ def add(request, shelf_id):
         {'form': form},
         context_instance=RequestContext(request),
     )
+
+def order_more(request, product_id_str):
+    product_id = int(product_id_str)
+    product = Product.objects.filter(id=product_id)[0]
+    return render_to_response(
+        'shoppinglist/templates/order-more.html',
+        {'product_id': product_id,
+         'name': product.name,
+         'desired': product.desired_amount,
+         'range1': range(2, 5),
+         'range2': range(5, 11)
+        },
+        context_instance=RequestContext(request),
+    )
+
+def increment(request, product_id_str, delta):
+    product_id = int(product_id_str)
+    product = Product.objects.filter(id=product_id)[0]
+    product.desired_amount += int(delta)
+    product.save()
+
+    return HttpResponseRedirect(reverse('shoppinglist.views.index'))
